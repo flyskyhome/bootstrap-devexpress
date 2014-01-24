90bcf994500a2d55283a10d13712bc838176eb75
flyskyhome <5likevideo@gmail.com>
2014-01-24 10:30:23 +0800

    新增yuidoc生成的 api文档

---
+++ apidocs/api.js
@@ -0,0 +1,38 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Modal",
+        "NavBar",
+        "Splitter",
+        "Tab"
+    ],
+    "modules": [
+        "Modal",
+        "NavBar",
+        "Splitter",
+        "Tab"
+    ],
+    "allModules": [
+        {
+            "displayName": "Modal",
+            "name": "Modal",
+            "description": "js插件:模态窗口"
+        },
+        {
+            "displayName": "NavBar",
+            "name": "NavBar",
+            "description": "ja插件:导航栏"
+        },
+        {
+            "displayName": "Splitter",
+            "name": "Splitter",
+            "description": "js插件:区域分割"
+        },
+        {
+            "displayName": "Tab",
+            "name": "Tab",
+            "description": "js插件:标签页"
+        }
+    ]
+} };
+});
