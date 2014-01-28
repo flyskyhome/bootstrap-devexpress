27a63f7edf90643e4b456b380e7fce3262aa8490
flyskyhome <5likevideo@gmail.com>
2014-01-28 10:36:03 +0800

    更新navbar部分说明

--- demo/templates/data/navbar.js
+++ demo/templates/data/navbar.js
@@ -25,32 +25,32 @@ var datainfo = {
 		'</ul>'
 	}, {
 		num: 3,
-		name: "不带图标(js)",
+		name: "创建(js)",
 		src:
-		'<div id="noicon"></div>	',
+		'<div id="noicon"></div>',
 		js:
 		'$("#noicon").bind("created", function (ev) {});'+
 		'$("#noicon").navbar({'+
 		'	items:['+
 		' 		{text:"栏目一",img:"url",url:"",key:"code",active:1},'+
-		'		{text:"栏目二",img:"url",url:"",key:"code"}'+
+		'		{text:"栏目二",img:"url",url:"",key:"code"},'+
 		'		{text:"栏目三",img:"url",url:"",key:"code"}'+
 		'	]'+
 		'});'
 	}, {
-		num: 3,
-		name: "图标(js)",
+		num: 4,
+		name: "激活(js)",
 		src:
-		'<div id="noicon"></div>	',
+		'<ul class="nav nav-pills nav-stacked">'+
+		'  <li class="active">'+
+		'    <a href="#">栏目一<i class="icon-chevron-right fr"></i></a>'+
+		'  </li>'+
+		'  <li><a href="#">栏目二<i class="icon-chevron-right fr"></i></a></li>'+
+		'  <li><a href="#">栏目三<i class="icon-chevron-right fr"></i></a></li>'+
+		'</ul>',
 		js:
-		'$("#noicon").bind("created", function (ev) {});'+
-		'$("#noicon").navbar({'+
-		'	items:['+
-		' 		{text:"栏目一",img:"url",url:"",key:"code",active:1},'+
-		'		{text:"栏目二",img:"url",url:"",key:"code"}'+
-		'		{text:"栏目三",img:"url",url:"",key:"code"}'+
-		'	]'+
-		'});'
+		'//激活第三个栏目：执行的应该是希望被激活那个li'+
+		'$("#tab4 li").eq(2).navbar("active");'
 	}]
 }
 
