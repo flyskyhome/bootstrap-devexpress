6f1922c448d97dad88642861354b5160dd077087
flyskyhome <5likevideo@gmail.com>
2014-02-12 12:27:16 +0800

    改进input 输入框
    1、添加class 非 span的支持

--- demo/templates/data/input.js
+++ demo/templates/data/input.js
@@ -5,13 +5,13 @@ var datainfo = {
 		name: "输入框",
 		active: "active",	
 		src:
-		'<input class="span4" type="text" id="inputEmail" placeholder="登录用户">'
+		'<input class="input-small" type="text" id="inputEmail" placeholder="登录用户">'
 	},{
 		num: 2,
 		name: "输入框(后缀)",
 		src:
 		'<div class="input-append">'+
-		'	<input class="span6" id="prependedInput" type="text" placeholder="Username">'+
+		'	<input class="input-small" id="prependedInput" type="text" placeholder="Username">'+
 		'	<span class="add-on"><button class="btn"><i class="icon-th"></i></button></span>'+
 		'	<span class="add-on"><button class="btn disabled"><i class="icon-th"></i></button></span>'+
 		'	<span class="add-on"><button class="btn"><i class="icon-time"></i></button></span>'+
@@ -26,26 +26,20 @@ var datainfo = {
 		'	<span class="add-on"><button class="btn disabled"><i class="icon-th"></i></button></span>'+
 		'	<span class="add-on"><button class="btn"><i class="icon-fullscreen"></i></button></span>'+
 		'	<span class="add-on"><button class="btn disabled"><i class="icon-chevron-down"></i></button></span>'+
-		'	<input class="span6" id="prependedInput" type="text" placeholder="Username">'+
+		'	<input class="input-small" id="prependedInput" type="text" placeholder="Username">'+
 		'</div>'
 	},{
 		num: 4,
 		name: "输入框(齐排)",
 		src:
-		'<input class="span2" type="text" id="inputEmail" placeholder="登录用户">											'+
-		'<div class="input-append">'+
-		'	<input class="span6" id="prependedInput" type="text" placeholder="Username">'+
-		'	<span class="add-on"><button class="btn"><i class="icon-th"></i></button></span>'+
-		'	<span class="add-on"><button class="btn disabled"><i class="icon-th"></i></button></span>'+
-		'	<span class="add-on"><button class="btn"><i class="icon-time"></i></button></span>'+
-		'	<span class="add-on" style="border-right:1px solid #9f9f9f"><button class="btn disabled"><i class="icon-chevron-down"></i></button></span>'+
+		'<input class="input-small" type="text" id="inputEmail" placeholder="登录用户">'+
+		'<div class="ml5 input-append">'+
+		'	<input class="input-small" id="prependedInput" type="text" placeholder="Username">'+
+		'	<span class="add-on add-on-fixedlast4ie78"><button class="btn"><i class="icon-chevron-down"></i></button></span>'+
 		'</div>'+
-		'<div class="ml10 input-prepend">'+
-		'	<span class="add-on"><button class="btn"><i class="icon-th"></i></button></span>'+
-		'	<span class="add-on"><button class="btn disabled"><i class="icon-th"></i></button></span>'+
-		'	<span class="add-on"><button class="btn"><i class="icon-fullscreen"></i></button></span>'+
-		'	<span class="add-on"><button class="btn disabled"><i class="icon-chevron-down"></i></button></span>'+
-		'	<input class="span6" id="prependedInput" type="text" placeholder="Username">'+
+		'<div class="ml5 input-prepend">'+
+		'	<span class="add-on"><button class="btn"><i class="icon-chevron-down"></i></button></span>'+
+		'	<input class="input-small" id="prependedInput" type="text" placeholder="Username">'+
 		'</div>',
 		todo:
 		'1、合适的控制每个输入框的宽度'
